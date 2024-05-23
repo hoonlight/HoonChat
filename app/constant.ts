b99f73f4021b2b3bf9d4fec6abbaedcda1c11c13
hoonlight <kihoon.kim.dev@gmail.com>
2024-05-23 12:41:29 +0900

    Update Repo

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const OWNER = "hoonlight";
+export const REPO = "HoonChat";
+export const REPO_URL = `https://github.com/${OWNER}/${REPO}/tree/custom`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
